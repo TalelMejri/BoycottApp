2a21ddbd7045c03c74b89b3a9deef40d9592ad83
TalelMejri <talelmejri8@gmail.com>
2023-12-27 20:53:08 +0100

    api for category

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix'=>'/category'],function(){
+    Route::post('/AddCategory',[CategoryController::class,'AddCategory']);
+    Route::get('/AllCategory',[CategoryController::class,'AllCategory']);
+    Route::delete('/DeleteCategory/{id}',[CategoryController::class,'DeleteCategory']);
+    Route::get('/EditCategory/{id}',[CategoryController::class,'EditCategory']);
+    Route::put('/UpdateCategory/{id}',[CategoryController::class,'UpdateCategory']);
 });
