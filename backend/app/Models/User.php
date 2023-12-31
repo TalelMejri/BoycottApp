91e15d5595448571ef7520da8ec3fa233e95fb00
ghassen <ghassenahmed74@gmail.com>
2023-12-31 18:12:15 +0100

    add columns to model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -17,11 +17,8 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['nom', 'prenom', 'email', 'password', 'photo'];
+
 
     /**
      * The attributes that should be hidden for serialization.
