7f126eadd814838f606398a7edd0da2fc6108f17
TalelMejri <talelmejri8@gmail.com>
2024-01-01 22:27:52 +0100

    add bloc signup

---
+++ mobile/lib/Features/Auth/presentation/bloc/signup/signup_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'signup_event.dart';
+part 'signup_state.dart';
+
+class SignupBloc extends Bloc<SignupEvent, SignupState> {
+  SignupBloc() : super(SignupInitial()) {
+    on<SignupEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
---
+++ mobile/lib/Features/Auth/presentation/bloc/signup/signup_event.dart
@@ -0,0 +1,4 @@
+part of 'signup_bloc.dart';
+
+@immutable
+sealed class SignupEvent {}
