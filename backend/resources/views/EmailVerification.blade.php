77179989753156016a325e17b4d338b23e82d95d
ghassen <ghassenahmed74@gmail.com>
2024-01-08 22:03:27 +0100

    add templates

---
+++ backend/resources/views/EmailVerification.blade.php
@@ -0,0 +1,14 @@
+@component('mail::message')
+<p style="color:#000;font-weight:600">
+    Hi {{$user->LastName}}<br>
+    We're happy you signed up for Boycott.To start<br>
+    exploring the App.<br>
+    please confirm your emails address.
+</p>
+ @component('mail::panel')
+ <p style="text-align:center">Token Verification</p>
+      <br>
+    <button style="color: green">{{$user->email_token}}</button>
+ @endcomponent
+ <p style="text-align:center"> Matrix Masters Team</p>
+@endcomponent
