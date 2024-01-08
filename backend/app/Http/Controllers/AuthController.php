92bb12aec17bf5e993acfef6605d17164c44f839
ghassen <ghassenahmed74@gmail.com>
2024-01-08 22:02:47 +0100

    add methods

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\SendTokenPassword;
+use App\Mail\VerifyMail;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class AuthController extends Controller
 {
@@ -31,4 +34,68 @@ class AuthController extends Controller
             return response()->json(['message' => 'Invalid credentials'], 401);
         }
     }
+    public function EnvoyerTokenEmail(String $email){
+        $user=User::where('email',$email)->first();
+        $token=mt_rand(10000,99999);
+
+        if($user->email_token == null){
+            // Mail::to($user->email)->send(new VerifyMail($user));
+            $user->email_token=$token;
+            $user->save();
+            return response()->json(['data'=>"Token has been sended"],200);
+        }else{
+            return response()->json(['data'=>"email already verified"],201);
+        }
+    }
+
+    public function VerifyEmail(String $email,Request $request){
+        $user=User::where('email',$email)->first();
+
+        if($request->token == $user->email_token){
+            $user->markEmailAsVerified();
+            $user->email_verified_at=now();
+            return response()->json(['data'=>"Email has been verified"],200);
+        }else{
+            return response()->json(['data'=>"Token invalid."],404);
+        }
+    }
+
+
+    public function renvoyer(String $email){
+        $user=User::where('email',$email)->first();
+        if($user->hasVerifiedEmail()){
+            return response()->json(['data'=>"email already verified"],200);
+        }
+        Mail::to($user->email)->send(new VerifyMail($user));
+        return response()->json(['data'=>"Link verified send with success"],200);
+    }
+
+
+    public function ForgotPassword(String $email){
+
+        $user=User::where('email',$email)->firstOrFail();
+        $token=mt_rand(10000,99999);
+        $user->password_token=$token;
+        $user->password_token_send_at=now();
+        $user->save();
+        Mail::to($email)->send(new SendTokenPassword($token));
+        return response()->json(["data"=>"Code Send To Your Email successfully"],200);
+   }
+
+   public function ChangerPassword(Request $request){
+    $user=User::where("email",$request->email)->where("password_token",$request->token)->first();
+    if($user){
+       if($user->password_token_send_at > now()->addHour()){
+         return response()->json(["data"=>"Token Expired"],404);
+        }
+        $user->update([
+            "password"=>bcrypt($request->password),
+            "password_token_send_at"=>null,
+            "password_token"=>null
+        ]);
+        return response()->json(["data"=>"Password Changed With success"],200);
+    }else{
+        return response()->json(["data"=>"Email Or Token not found"],404);
+    }
+   }
 }
