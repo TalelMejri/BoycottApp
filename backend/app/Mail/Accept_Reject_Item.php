6d42e04bc8d1a80291d15f847e22a6b7d04d71a8
TalelMejri <talelmejri8@gmail.com>
2024-02-20 16:36:55 +0100

    Accept_Reject_Item Mailable

---
+++ backend/app/Mail/Accept_Reject_Item.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class Accept_Reject_Item extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $message;
+    public function __construct($message)
+    {
+       $this->message=$message;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Accept Reject Item',
+        );
+    }
+
+
+    public function build()
+    {
+        return $this->subject('Alert From BoycottApp')
+                     ->markdown('AcceptRejectItem');
+    }
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'view.name',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
