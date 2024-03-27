8317a1fe355608efa3e14dc6a719a0d646b2617e
TalelMejri <talelmejri8@gmail.com>
2024-03-27 01:51:20 +0100

    CustomScaffold

---
+++ mobile/lib/Core/widgets/custom_scaffold.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomScaffold extends StatelessWidget {
+  const CustomScaffold({super.key, this.child});
+  final Widget? child;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            'images/back.jpg',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          SafeArea(
+            child: child!,
+          ),
+        ],
+      ),
+    );
+  }
+}
